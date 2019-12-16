7844e905238ca013e38531b10f4f403c2c16668e
ivan.rovenskiy <ivan.rovenskiy@team.wrike.com>
2019-12-17 02:04:38 +0300

    added some styles and mouse actions listener on battle grid

--- src/main/java/ru/vsu/controllers/GameFieldController.java
+++ src/main/java/ru/vsu/controllers/GameFieldController.java
@@ -2,7 +2,11 @@ package ru.vsu.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.HPos;
+import javafx.geometry.VPos;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.RowConstraints;
@@ -16,6 +20,10 @@ import ru.vsu.config.CurrentGameSettings;
 public class GameFieldController {
     @FXML
     private GridPane playerGridPane;
+    @FXML
+    private GridPane opponentGridPane;
+    @FXML
+    private TextArea battleLogTextArea;
 
     @FXML
     void backToMenuAction(ActionEvent event) {
@@ -24,10 +32,14 @@ public class GameFieldController {
 
     @FXML
     void initialize() {
-        initPane(playerGridPane);
+        initPlayersGridPane(playerGridPane);
+        addEmptyCellsToGridPane(playerGridPane);
+
+        initPlayersGridPane(opponentGridPane);
+        addEmptyCellsToGridPane(opponentGridPane);
     }
 
-    private void initPane(GridPane gridPane) {
+    private void initPlayersGridPane(GridPane gridPane) {
         gridPane.getColumnConstraints().removeAll(gridPane.getColumnConstraints());
         gridPane.getRowConstraints().removeAll(gridPane.getRowConstraints());
 
@@ -35,13 +47,30 @@ public class GameFieldController {
         for (int column = 0; column < currentGameSettings.getGameFieldSize() + 1; column++) {
             gridPane.getColumnConstraints().add(new ColumnConstraints() {{
                 setPercentWidth(100d / (currentGameSettings.getGameFieldSize() + 1));
+                setHalignment(HPos.CENTER);
             }});
             gridPane.getRowConstraints().add(new RowConstraints() {{
                 setPercentHeight(100d / (currentGameSettings.getGameFieldSize() + 1));
+                setValignment(VPos.CENTER);
             }});
             if (column > 0) {
                 gridPane.add(new Label(String.valueOf(column)), column, 0);
                 gridPane.add(new Label(String.valueOf(column)), 0, column);
+            }
+        }
+    }
+
+    private void addEmptyCellsToGridPane(GridPane gridPane) {
+        final CurrentGameSettings currentGameSettings = CurrentGameSettings.getCurrentGameSettings();
+        for (int row = 1; row < currentGameSettings.getGameFieldSize() + 1; row++) {
+            for (int column = 1; column < currentGameSettings.getGameFieldSize() + 1; column++) {
+                gridPane.add(new Button() {{
+                    setMaxHeight(Double.MAX_VALUE);
+                    setMaxWidth(Double.MAX_VALUE);
+                    getStyleClass().add("common-grid-cell");
+                    setOnMouseEntered(mouseEvent -> getStyleClass().add("under-mouse-grid-cell"));
+                    setOnMouseExited(mouseEvent -> getStyleClass().remove("under-mouse-grid-cell"));
+                }}, column, row);
             }
         }
     }
